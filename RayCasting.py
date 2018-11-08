ce30f2e101be408cc26b69f2be5dd0c373bb9651
Joaquin Font <fontj@lafayette.edu>
2018-11-08 01:22:21 -0500

    RayCasting with map fully functional

--- RayCasting.py
+++ RayCasting.py
@@ -1,11 +1,14 @@
 import sys
 from numpy import *
 from matplotlib.pyplot import *
+from KMLtoXYZ import *
 
-def distanceray(pos):
+def distanceray(pos, array):
     
     # Points of array (Begin and end with the same point)
-    P = array([[-1.0,1.0],[1.0,1.0],[1.0,-1.0],[-1.0,-1.0],[-1.0,1.0]])
+
+    print 'array', array
+    P = double(array)
 
     dist_angle = zeros(360)
 
@@ -13,47 +16,33 @@ def distanceray(pos):
 
         angle = angle_deg/180.0*3.1416
 
-        dist_vec = zeros(len(P)-1)
+        dist_vec = zeros(len(P[0])-1)
 
-        for seg in range(1,len(P)):
+        for seg in range(1,len(P[0])):
 
             # If segment is perpendicular:
-            if (P[seg,0]==P[seg-1,0]):
+            if (P[0,seg]==P[0,seg-1]):
 
-                xint = P[seg,0]
+                xint = P[0,seg]
 
                 yint = tan(angle)*(xint-pos[0])+pos[1]
 
                 #print seg    
 
-                #print xint, yint
-
             # If segment is not perpendicular:
             else:
 
-                dydx_seg = abs((P[seg,1]-P[seg-1,1])/(P[seg,0]-P[seg-1,0]))
+                dydx_seg = ((P[1,seg]-P[1,seg-1])/(P[0,seg]-P[0,seg-1]))
 
-                #print 'dydx', dydx_seg
+                xint = (dydx_seg*P[0,seg-1]-P[1,seg-1]-tan(angle)*pos[0]+pos[1])/(dydx_seg-tan(angle))
 
-                xint = (dydx_seg*P[0,0]-P[0,1]-tan(angle)*pos[0]+pos[1])/(dydx_seg-tan(angle))
+                yint = (P[1,seg]-P[1,seg-1])/(P[0,seg]-P[0,seg-1])*(xint-P[0,seg-1])+P[1,seg-1]
 
-                yint = (P[seg,1]-P[seg-1,1])/(P[seg,0]-P[seg-1,0])*(xint-P[seg-1,0])+P[seg-1,1]
-
-                #print seg
-
-                #print xint, yint
+            #print xint, yint
 
             # Check that the ray intercepts the segment
-            if ((((xint >= P[seg-1,0]) and (xint <= P[seg,0])) or ((xint >= P[seg,0]) and (xint <= P[seg-1,0]))) and (((yint >= P[seg-1,1]) and (yint <= P[seg,1])) or ((yint >= P[seg,1]) and (yint <= P[seg-1,1])))):
+            if ((((xint >= P[0,seg-1]) and (xint <= P[0,seg])) or ((xint >= P[0,seg]) and (xint <= P[0,seg-1]))) and (((yint >= P[1,seg-1]) and (yint <= P[1,seg])) or ((yint >= P[1,seg]) and (yint <= P[1,seg-1])))):
                 dist_vec[seg-1] = sqrt((yint-pos[1])**2+(xint-pos[0])**2)
-
-
-
-                #print yint-pos[1]
-
-                #print xint-pos[0]
-
-                #print dist_vec[seg-1]
 
             else:
 
@@ -62,21 +51,22 @@ def distanceray(pos):
                 dist_vec[seg-1] = NaN
 
             # Check that the interception is for positive ray
-            # if (angle_deg>90 and angle_deg<270):
+            if (angle_deg>=90 and angle_deg<270):
+                if (xint>pos[0]):
 
-            #     if (xint>pos[0]):
+                    #print 'Negative Ray'
+                    dist_vec[seg-1] = NaN
 
-            #         print 'Negative Ray'
-            #         dist_vec[seg-1] = NaN
+            else:
+                if (xint<pos[0]):
+                    dist_vec[seg-1] = NaN
 
-            #else:
 
-                #print 'nan'
-
-               # if (xint<pos[0]):
-                    #dist_vec[seg-1] = NaN
+            #print dist_vec
 
         dist_angle[angle_deg] = nanmin(dist_vec)
+
+
 
         if (dist_angle[angle_deg]>100):
             dist_angle[angle_deg] = 100
@@ -85,11 +75,11 @@ def distanceray(pos):
 
     angle_plot = linspace(0,360,360)
 
-    figure()
-    plot(angle_plot,dist_angle)
-    title('Ray Casting')
-    xlabel('Angle (deg)')
-    ylabel('Minimum Distance (m)')
+    # figure()
+    # plot(angle_plot,dist_angle)
+    # title('Ray Casting')
+    # xlabel('Angle (deg)')
+    # ylabel('Minimum Distance (m)')
 
     #show()
 
@@ -128,26 +118,68 @@ def ProbDist(pos, psi1, q_dist = 1.0,q_dangle = 1/1000.0):
 
     #show() 
 
-    print Prob1
-
     return Prob1
 
+def MapRaycasting(pos):
 
+    sight_dist = 60.0
+
+    f = 'Test2.kml'
+
+    Trees = KMLtoXYZ(f)
+
+    MinDist = []
+
+    for ind in range(0,len(Trees)):
+        MinDist.append(distanceray(pos, Trees[ind]))
+
+    MinDist_Vec = zeros(len(MinDist[0]))
+
+    d = zeros(len(MinDist))
+
+    for ind in range(0,len(MinDist[0])):
+        for ind1 in range(0,len(MinDist)):
+            d[ind1] = MinDist[ind1][ind]
+
+        MinDist_Vec[ind] = nanmin(d) 
+
+        if isnan(MinDist_Vec[ind]):
+            MinDist_Vec[ind] = sight_dist
+        if MinDist_Vec[ind]>sight_dist:
+            MinDist_Vec[ind] = sight_dist
+
+    print MinDist_Vec
+
+
+    angle_plot = linspace(0,360,360)
+
+    figure()
+    plot(angle_plot,MinDist_Vec)
+    title('Ray Casting')
+    xlabel('Angle (deg)')
+    ylabel('Minimum Distance (m)')
+
+    figure()
+    seg = zeros((2,2))
+    print seg
+    for ind in range(0,len(Trees)):
+        plot(Trees[ind][0],Trees[ind][1])
+    for ind in range(0,360):
+        seg[0][0] = pos[0]
+        seg[0][1] = pos[0]+MinDist_Vec[ind]*cos(ind*3.1416/180)
+        seg[1][0] = pos[1]
+        seg[1][1] = pos[1]+MinDist_Vec[ind]*sin(ind*3.1416/180)
+        plot(seg[0][:],seg[1][:],'k')
+    plot(pos[0],pos[1],'ro')
+    axis('equal')
+    xlabel('X (m)')
+    ylabel('Y (m)')
+
+
+    show()
 
 
 
 if __name__=='__main__':
 
-    Prob = ProbDist([0,0],0)
-
-    print Prob
-
-    vec=zeros(100000)
-
-    for k in range(0,100000):
-
-        vec[k] = random.choice(arange(0,360),p=list(Prob))
-
-    hist(vec,360)
-
-    show()
+    MapRaycasting([100.0,0.0])
