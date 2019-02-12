5f63d992a6e9ae74128f6f527194bdf7651ac79b
Joaquin Font <fontj@lafayette.edu>
2019-02-12 16:17:24 -0500

    Ready to train CV deer at 50m sight distance

--- TrainDeer_MPC_Map.py
+++ TrainDeer_MPC_Map.py
@@ -65,7 +65,7 @@ def TestDeer_MPC(deer_ind, n, agent):
 
     for k_1 in range(0,n):
 
-        MPCDistance = 80.0
+        MPCDistance = 50.0
         setSpeed = 25.0
         x_deer = 80.0
         x_car = 0.0
@@ -429,5 +429,5 @@ def FirstGen():
 
 if __name__=='__main__':
 
-    FirstGen()
+    demo()
 
