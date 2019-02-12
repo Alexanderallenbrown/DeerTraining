07d3e74c772ce85fff5709ccb12f7b850a31c1b1
Joaquin Font <fontj@lafayette.edu>
2019-02-11 19:41:09 -0500

    Collision Test added

--- MPC_F_braking_KinCar.py
+++ MPC_F_braking_KinCar.py
@@ -269,10 +269,10 @@ def demo_GAdeer():
 
     swerveDistance = 50.0
     setSpeed = 25.0
-    x_car = 20.0
-    x_deer = 100.0
+    x_car = 0.0
+    x_deer = 80.0
 
-    deer_ind = '1100101100110111011110101'
+    deer_ind = '0101010000110111101100000'
 
     deer_ind = BinaryConversion(deer_ind)
 
@@ -306,7 +306,7 @@ def demo_GAdeer():
 
     #MPC = MPC_F(q_lane_error = 10.0,q_obstacle_error = 5000000.0,q_lateral_velocity=0.00,q_steering_effort=0.0,q_accel = 0.005)
     weight = 10.0
-    MPC = MPC_Fb(q_lane_error = weight,q_obstacle_error =200.0/weight*10,q_lateral_velocity=1.0,q_steering_effort=1.0,q_accel = 0.005,predictionmethod='CV')
+    MPC = MPC_Fb(q_lane_error = weight,q_obstacle_error =0.0/weight*10,q_lateral_velocity=1.0,q_steering_effort=1.0,q_accel = 0.005,predictionmethod='CV')
 
     actual_steervec = zeros(len(t))
     command_steervec = zeros(len(t))
@@ -522,8 +522,8 @@ def demo_CVdeer():
     deer_x = 80.0
     car_x = 0.0
 
-    angle = -90
-    speed = 6
+    angle = -88
+    speed = 10
     # Initiate process
     deer = CV_Deer()
     deer.x_Deer = deer_x
