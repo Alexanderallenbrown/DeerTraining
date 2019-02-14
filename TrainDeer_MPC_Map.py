af800dca8f9480edc672a1c7bd5c6bb5c17cb095
Joaquin Font <fontj@lafayette.edu>
2019-02-14 01:44:52 -0500

    Ready tp train deer in a single tree at 25ms

--- TrainDeer_MPC_Map.py
+++ TrainDeer_MPC_Map.py
@@ -66,7 +66,7 @@ def TestDeer_MPC(deer_ind, n, agent):
     for k_1 in range(0,n):
 
         MPCDistance = 50.0
-        setSpeed = 25.0
+        setSpeed = 23.0
         x_deer = 80.0
         x_car = 0.0
         KML = False
