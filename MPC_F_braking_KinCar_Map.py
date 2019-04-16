fb79b5ded24f18d29f08ed98e1d11f06fa1fd49d
Joaquin Font <fontj@lafayette.edu>
2019-04-16 18:01:33 -0400

    TraiFile closing added

--- MPC_F_braking_KinCar_Map.py
+++ MPC_F_braking_KinCar_Map.py
@@ -278,16 +278,17 @@ class MPC_Fb:
 
 def demo_GAdeer():
 
-    fakemap = 'real_tree_wismer_7'
+    fakemap = 'constant_4'
     swerveDistance = 50.0
-    setSpeed = 10.0
-    x_car = 40.0
+    setSpeed = 25.0
+    x_car = 0.0
     x_deer = x_car + 80.0
 
     deer_ind = '0011010011000001111000000'
     deer_ind = '0100010000000111111100000'
     deer_ind = '0000000000000001000100100' #xcar 0, speed 21, generation 17
     deer_ind = '0010101111000001010000011'
+    deer_ind = '0101100011000100100000110'
 
     deer_ind = BinaryConversion_Escape(deer_ind)
 
@@ -597,6 +598,15 @@ def demo_GAdeer():
         trees_ind = True
     if fakemap == 'real_tree_wismer_8':
         trees = patches.Rectangle((100,-98),80,90,fc='g')
+        trees_ind = True
+    if fakemap == 'constant_4':
+        trees = patches.Rectangle((-100,-94),400,90,fc='g')
+        trees_ind = True
+    if fakemap == 'constant_7':
+        trees = patches.Rectangle((-100,-97),400,90,fc='g')
+        trees_ind = True
+    if fakemap == 'constant_10':
+        trees = patches.Rectangle((100,-100),400,90,fc='g')
         trees_ind = True
 
 
